03210c1946918559feed121f372b4ea6954a52c4
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-15 16:17:18 +0200

    Fetching meals via Http. closes #29

---
+++ 17-adding-http-and-forms-to-the-food-order-app/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,79 @@
+import {useEffect, useState} from 'react'
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const DUMMY_MEALS = [
+    {
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 22.99,
+    },
+    {
+        id: 'm2',
+        name: 'Schnitzel',
+        description: 'A german specialty!',
+        price: 16.5,
+    },
+    {
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
+    },
+    {
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
+    },
+];
+
+const AvailableMeals = () => {
+
+    const [meals, setMeals] = useState([])
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://taks-46710-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+            const responseDATA = await response.json()
+
+            const loadedMeals = []
+
+            for (const key in responseDATA) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseDATA[key].name,
+                    description: responseDATA[key].description,
+                    price: responseDATA[key].price
+                })
+            }
+
+            setMeals(loadedMeals)
+        }
+
+        fetchMeals()
+
+    }, [])
+
+    const mealsList = meals.map((meal) => (
+        <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
+
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
+        </section>
+    );
+};
+
+export default AvailableMeals;
