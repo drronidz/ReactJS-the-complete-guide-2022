36c3452385cc8145db9eac9eea826c5c7cbadad0
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-17 01:11:35 +0200

    Reading & Dispatching from a new slice (authentication process). closes #58

--- 18-diving-into-redux/src/App.js
+++ 18-diving-into-redux/src/App.js
@@ -2,17 +2,21 @@ import Counter from './components/Counter';
 import React, {Fragment} from "react";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import {useSelector} from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 
 function App() {
-  return (
-      <Fragment>
-        <Header/>
-        <Auth/>
-        <Counter />
-      </Fragment>
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+    return (
+        <Fragment>
+            <Header/>
+            { !isAuthenticated && <Auth/> }
+            { isAuthenticated && <UserProfile/> }
+            <Counter/>
+        </Fragment>
 
-  );
+    );
 }
 
 export default App;
