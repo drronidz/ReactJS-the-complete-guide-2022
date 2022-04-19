f238d912b96a2a47c574fef518ab5ef2087cf582
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-19 03:00:57 +0200

    Refreshing practices Part 2. closes #64

--- 19-advanced-redux/src/components/Shop/Products.js
+++ 19-advanced-redux/src/components/Shop/Products.js
@@ -1,19 +1,30 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+    { id: 'p1', price: 6, title: 'FIRST BOOK', description: 'The first book I ever wrote' },
+    { id: 'p2', price: 9, title: 'SECOND BOOK', description: 'The second book I ever wrote' },
+    { id: 'p3', price: 12, title: 'THIRD BOOK', description: 'The third book I ever wrote' }
+]
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map(product =>
+                    <ProductItem
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        price={product.price}
+                        description={product.description}
+                    />
+                    )}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
--- 19-advanced-redux/src/store/index.js
+++ 19-advanced-redux/src/store/index.js
@@ -1,13 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
 
 
 const store = configureStore({
     reducer : {
-        ui : uiSlice.reducer
+        ui : uiSlice.reducer,
+        cart: cartSlice.reducer
     }
 })
 
 export const uiActions = uiSlice.actions
+export const cartActions = cartSlice.actions
 
 export default store
