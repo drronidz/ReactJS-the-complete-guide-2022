23b426421f3dbb6f32c3d61768e347fc28edc0b0
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-21 01:46:53 +0200

    Using an Action Creator Thunk. closes #72

--- 19-advanced-redux/src/App.js
+++ 19-advanced-redux/src/App.js
@@ -3,8 +3,8 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useDispatch, useSelector} from "react-redux";
 import React, {Fragment, useEffect} from "react";
-import {uiActions} from "./store";
 import Notification from "./components/UI/Notification";
+import {sendCartDATA} from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -15,49 +15,11 @@ function App() {
     const notification = useSelector((state) => state.ui.notification);
 
     useEffect(() => {
-        const sendCartData = async () => {
-            dispatch(
-                uiActions.showNotification({
-                    status: 'pending',
-                    title: 'Sending...',
-                    message: 'Sending cart data!',
-                })
-            );
-            const response = await fetch(
-                'https://taks-46710-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-                {
-                    method: 'PUT',
-                    body: JSON.stringify(cart),
-                }
-            );
-
-            if (!response.ok) {
-                throw new Error('Sending cart data failed.');
-            }
-
-            dispatch(
-                uiActions.showNotification({
-                    status: 'success',
-                    title: 'Success!',
-                    message: 'Sent cart data successfully!',
-                })
-            );
-        };
-
         if (isInitial) {
-            isInitial = false;
-            return;
+            isInitial = false
+            return
         }
-
-        sendCartData().catch((error) => {
-            dispatch(
-                uiActions.showNotification({
-                    status: 'error',
-                    title: 'Error!',
-                    message: 'Sending cart data failed!',
-                })
-            );
-        });
+        dispatch(sendCartDATA(cart))
     }, [cart, dispatch]);
 
     return (
