83ec587bd1b9d552ad12c1df47bf8114439de55d
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-22 17:14:22 +0200

    Working with Nested Routes. closes #86

--- 20-building-multi-page-spa-with-react-router/src/pages/Welcome.js
+++ 20-building-multi-page-spa-with-react-router/src/pages/Welcome.js
@@ -1,7 +1,15 @@
 import React from "react";
+import {Route, Switch} from 'react-router-dom'
 
 const Welcome = () => {
-    return <h1>This is the Welcome page</h1>
+    return (
+        <section>
+            <h1>This is the Welcome page</h1>
+            <Route path={"/welcome/new-user"}>
+                <h1>Welcome, new User!</h1>
+            </Route>
+        </section>
+    )
 }
 
 export default Welcome
