e7f10da1dabb5593fe45f5c926bc8d3e4883730c
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-22 16:30:53 +0200

    Defining & Using Routes. closes #80

--- 20-building-multi-page-spa-with-react-router/src/App.js
+++ 20-building-multi-page-spa-with-react-router/src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+import {Route} from 'react-router-dom'
+import React from "react";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
+const App = () => {
+    return (
+        <div>
+            <Route path="/welcome">
+                <Welcome/>
+            </Route>
+            <Route path="/products">
+                <Products/>
+            </Route>
+        </div>
+    );
 }
 
+
 export default App;
+
+// our-domain.com/welcome => Component A (Welcome Component)
+// our-domain.com/products => Component B (Products Component)
