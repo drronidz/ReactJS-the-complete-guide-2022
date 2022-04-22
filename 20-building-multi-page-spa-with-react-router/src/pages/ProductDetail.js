3e4b6fe61aac3456a74e237df372a5617453937d
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-22 16:59:04 +0200

    Extracting Route Params. closes #84

--- 20-building-multi-page-spa-with-react-router/src/pages/ProductDetail.js
+++ 20-building-multi-page-spa-with-react-router/src/pages/ProductDetail.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useParams } from 'react-router-dom'
 
 const ProductDetail = () => {
+    const params = useParams()
+    console.log(params.productId)
+
     return (
         <section>
             <h1>This Product Details</h1>
+            <p>{params.productId}</p>
         </section>
     )
 }
