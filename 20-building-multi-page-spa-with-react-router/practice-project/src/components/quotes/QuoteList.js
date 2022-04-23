f543b9c70b017b351823fa9158ef85ad6013c041
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-23 16:47:40 +0200

    Working with QueryParams (Sorting). closes #97

--- 20-building-multi-page-spa-with-react-router/practice-project/src/components/quotes/QuoteList.js
+++ 20-building-multi-page-spa-with-react-router/practice-project/src/components/quotes/QuoteList.js
@@ -1,23 +1,43 @@
-import { Fragment } from 'react';
+import React, {Fragment} from 'react';
+import { useHistory, useLocation } from 'react-router-dom'
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
+import sortQuotes from "./utils";
 
 const QuoteList = (props) => {
-  return (
-    <Fragment>
-      <ul className={classes.list}>
-        {props.quotes.map((quote) => (
-          <QuoteItem
-            key={quote.id}
-            id={quote.id}
-            author={quote.author}
-            text={quote.text}
-          />
-        ))}
-      </ul>
-    </Fragment>
-  );
+    const history = useHistory()
+    const location = useLocation()
+
+    const queryParams = new URLSearchParams(location.search)
+
+    const isSortingASC = queryParams.get('sort') === 'asc'
+
+    const sortedQuotes = sortQuotes(props.quotes, isSortingASC)
+
+    const changeSortingHandler = () => {
+        history.push('/quotes?sort=' + (isSortingASC ? 'desc' : 'asc'))
+    }
+
+    return (
+        <Fragment>
+            <div className={classes.sorting}>
+                <button onClick={changeSortingHandler}>
+                    Sort {isSortingASC ? 'Descending' : 'Ascending'}
+                </button>
+            </div>
+            <ul className={classes.list}>
+                {sortedQuotes.map((quote) => (
+                    <QuoteItem
+                        key={quote.id}
+                        id={quote.id}
+                        author={quote.author}
+                        text={quote.text}
+                    />
+                ))}
+            </ul>
+        </Fragment>
+    );
 };
 
 export default QuoteList;
