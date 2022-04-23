8c9cb1d4bc66ba187c4ef47d7df76bdf0d1ace7c
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-23 02:38:52 +0200

    Practicing Redirecting and Extracting Params. closes #89.

--- 20-building-multi-page-spa-with-react-router/practice-project/src/pages/QuoteDetail.js
+++ 20-building-multi-page-spa-with-react-router/practice-project/src/pages/QuoteDetail.js
@@ -1,7 +1,15 @@
 import React from "react";
+import {useParams} from 'react-router-dom'
 
 const QuoteDetail = () => {
-    return <h1>Quote Detail Page</h1>
+    const params = useParams()
+    const quoteId = params.quoteId
+    return (
+        <section>
+            <h1>Quote Detail Page</h1>
+            <h2>Quote id is : {quoteId}</h2>
+        </section>
+        )
 }
 
 export default QuoteDetail
