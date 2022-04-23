dd04b90384dd4945c61a23bd397aac237c7b0c67
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-23 15:57:28 +0200

    Implementing Programmatic (Imperative) Navigation. closes #94

--- 20-building-multi-page-spa-with-react-router/practice-project/src/pages/NewQuote.js
+++ 20-building-multi-page-spa-with-react-router/practice-project/src/pages/NewQuote.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useHistory } from 'react-router-dom'
+
+// Components
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+    const history = useHistory()
+
+
     const addQuoteHandler = quoteDATA => {
         console.log(quoteDATA)
+        console.log(history)
+        history.push('/quotes')
     }
 
     return <QuoteForm onAddQuote={addQuoteHandler}/>
